e4528a76fe70479c9c86cc8951307ea0c794f9e8
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-03-04 22:40:01 +0700

    0.0.4
    Hoàn thành header cơ bản bằng Material UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
-import Button from "@mui/material/Button";
+//import Button from "@mui/material/Button";
+//<Button variant="contained">Hello world</Button>
+import * as UI from "@mui/material";
 
 export default function Header() {
   return (
     <>
-      <Button variant="contained">Hello worlddsfsa dfsd</Button>
+      <UI.Box sx={{ flexGrow: 1 }}>
+        <UI.AppBar position="static">
+          <UI.Toolbar>
+            <UI.Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Travel Site
+            </UI.Typography>
+
+            <UI.Button color="inherit">Home</UI.Button>
+            <UI.Button color="inherit">Booking</UI.Button>
+            <UI.Button color="inherit">Guides</UI.Button>
+            <UI.Button color="inherit">Destinations</UI.Button>
+            <UI.Button color="inherit">FAQ</UI.Button>
+          </UI.Toolbar>
+        </UI.AppBar>
+      </UI.Box>
     </>
   );
 }
