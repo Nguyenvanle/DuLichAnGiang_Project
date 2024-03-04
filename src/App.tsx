012ce4de727f3dfc38af8ddd245a328f1cc7e6e6
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-03-04 21:19:04 +0700

    0.0.2
    Cấu trúc những trang cơ bản của website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import Guides from "./pages/Guides";
+import Destinations from "./pages/Destinations";
+import FAQ from "./pages/FAQ";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+        <Booking />
+        <Guides />
+        <Destinations />
+        <FAQ />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
